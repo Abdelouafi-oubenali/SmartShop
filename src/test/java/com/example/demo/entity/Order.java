8cee27871aec2a1fdf4f671704076c5533030723
Abdelouafi <abdelouafioubenali9@gmail.com>
2025-11-24 22:31:27 +0100

    Travail sur les entités nécessaires du projet

---
+++ src/test/java/com/example/demo/entity/Order.java
@@ -0,0 +1,46 @@
+package com.example.demo.entity;
+
+
+import com.example.demo.enums.OrderStatus;
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+import java.util.Date;
+import java.util.List;
+
+
+@Data
+@Entity
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id  ;
+
+    @ManyToOne
+    @JoinColumn(name = "client_id")
+    private Client client ;
+
+    private LocalDateTime orderDate ;
+    private Double discount ;
+    private Double tax ;
+    private Double total ;
+
+
+    @ManyToOne
+    @JoinColumn(name = "promo_code_id")
+    private PromoCode promoCode ;
+
+    @Enumerated(EnumType.STRING)
+    private OrderStatus status ;
+
+    private Double remainingAmount ;
+
+
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
+    private List<OrderItem> items  ;
+
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
+    private List<Paiement> payments ;
+
+}
---
+++ src/test/java/com/example/demo/entity/Paiement.java
@@ -0,0 +1,42 @@
+package com.example.demo.entity;
+
+import com.example.demo.enums.PaymentStatus;
+import com.example.demo.enums.PaymentType;
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+import java.util.Date;
+
+@Data
+@Entity
+public class Paiement {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id ;
+
+    private Integer paymentNumber ;
+    private Double amount ;
+
+    @Enumerated(EnumType.STRING)
+    private PaymentType paymentType ;
+
+    private LocalDateTime paymentDate ;
+    private LocalDateTime depositDate ;
+
+    @Enumerated(EnumType.STRING)
+    private PaymentStatus status;
+
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    private Order order ;
+
+    private String reference ;
+
+    private String bank  ;
+
+
+
+
+
+}
